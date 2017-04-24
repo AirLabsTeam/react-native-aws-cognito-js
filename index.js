c9faf43fe4b873bb1b90ba4c7c9ddca6ead99d20
Jonathan Parsons <jonparsons@gmail.com>
2017-04-23 23:50:13 -0400

    Added initial setup files.

---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { RNAWSCognito } = NativeModules;
+
+export default RNAWSCognito;
---
+++ ios/RNAWSCognito.m
@@ -0,0 +1,11 @@
+#import "RNAWSCognito.h"
+
+@implementation RNAWSCognito
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
